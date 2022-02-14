276c283e1ddee414f851522105333aae369048f9
Thapanapong Sinprommat <kthapnp@gmail.com>
2022-02-14 18:17:36 +0700

    frontend conn to backend and get data

---
+++ react_frontend/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import Users from "./Users";
+
+function App() {
+  return (
+    <div className="">
+      <h2>User components</h2>
+      <Users />
+    </div>
+  );
+}
+
+export default App;
+
+/*
+ref:
+  https://www.youtube.com/watch?v=Oiczjew0faU
+  https://github.com/AjayYadavAi/InertiaTodo
+*/
